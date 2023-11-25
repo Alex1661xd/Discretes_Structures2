5f9f2f5d30864a4ced5f274d0ff2d6f8ee4378d4
Valentinag9 <1107839938@u.icesi.edu.co>
2023-11-25 11:49:42 -0500

    Entrega

--- src/main/java/com/example/demo/TestCases/TestGraphsController.java
+++ src/main/java/com/example/demo/TestCases/TestGraphsController.java
@@ -28,5 +28,45 @@ public class TestGraphsController extends TestCase {
         assertEquals("Conexiones de V2: V4", graphsController.showConnections(vertice));
     }
 
-    // Puedes agregar más casos de prueba según sea necesario
+
+    public void testGetInstance() {
+        GraphsController instance1 = GraphsController.getInstance();
+        GraphsController instance2 = GraphsController.getInstance();
+
+        // Verificar que ambas instancias sean la misma instancia
+        assertSame(instance1, instance2);
+    }
+
+
+    public void testAddGraph1() {
+        GraphsController graphController = GraphsController.getInstance();
+        graphController.setTypeGraph(1);
+
+        // Asegurarse de que no haya excepciones al agregar el grafo 1
+        assertDoesNotThrow(() -> graphController.addGraph());
+    }
+
+
+    public void testAddGraph2() {
+        GraphsController graphController = GraphsController.getInstance();
+        graphController.setTypeGraph(2);
+
+        // Asegurarse de que no haya excepciones al agregar el grafo 2
+        assertDoesNotThrow(() -> graphController.addGraph());
+    }
+
+
+    public void testShowDistanciasCortas() {
+        GraphsController graphController = GraphsController.getInstance();
+        graphController.setTypeGraph(1);
+
+        // Agregar nodos y conexiones al grafo1
+        graphController.addGraph();
+
+        // Asegurarse de que la distancia corta sea un número no negativo
+        int distancia = graphController.showDistanciasCortas("A", "B");
+        assertTrue(distancia >= 0);
+    }
+
+
 }
